f2f092d654ad35a08f497be2cae037431b3d391d
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2023-01-25 18:28:11 -0500

    Limelight create

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,8 +4,14 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class Limelight extends SubsystemBase {
+  /** Creates a new Limelight. */
+  public Limelight() {}
 
-
-
-
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
