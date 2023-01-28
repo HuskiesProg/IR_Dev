989478f87895abce3770db803214b86d4b707fd8
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2023-01-28 09:45:20 -0500

    corrected merge
    Corrected merge

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,10 +1,8 @@
 package frc.robot.subsystems;
 
-<<<<<<< Updated upstream
+
 import edu.wpi.first.math.filter.LinearFilter;
-=======
 import edu.wpi.first.math.geometry.Pose2d;
->>>>>>> Stashed changes
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
@@ -20,22 +18,9 @@ public class Limelight extends SubsystemBase {
   private NetworkTableEntry botpose = limelight.getEntry("botpose");
   private NetworkTableEntry stream = limelight.getEntry("stream");
 
-<<<<<<< Updated upstream
-
   public Limelight() {
     stream.setNumber(2);//Pour mettre l'image de la limelight en PiP
     SmartDashboard.putString("Pose", getPos().toString());
-    
-  }
-
-  public Pose3d getPos(){
-    double[] temp = { 0, 0, 0 ,0,0,0};//default for getEntry
-=======
-  private Pose3d test = new Pose3d();
-
-  public Limelight() {
-    stream.setNumber(2);//Pour mettre l'image de la limelight en PiP
-    
     
   }
 
@@ -47,17 +32,12 @@ public class Limelight extends SubsystemBase {
 
   public Pose3d getPos(){
     double[] temp = { 0,0,0,0,0,0};//default for getEntry
->>>>>>> Stashed changes
+
     double[] result = botpose.getDoubleArray(temp);
     Translation3d tran3d = new Translation3d(result[0], result[1], result[2]);
     Rotation3d r3d = new Rotation3d(result[3], result[4], result[5]);
     Pose3d p3d = new Pose3d(tran3d, r3d);
     
-<<<<<<< Updated upstream
-   return p3d;
-
-  } 
-=======
     return p3d;
   } 
 
@@ -67,5 +47,4 @@ public class Limelight extends SubsystemBase {
     return output;
 
   }
->>>>>>> Stashed changes
 }
