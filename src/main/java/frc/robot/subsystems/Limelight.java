b50479cfb5597cebe151e43e7b4426a8a2a643cf
Huskies Prog <huskies5439@polych.ca>
2023-02-01 16:58:11 -0500

    bot Pose marche

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,48 +1,54 @@
 package frc.robot.subsystems;
 
-
-import edu.wpi.first.math.filter.LinearFilter;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   private NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
-  private NetworkTable limelight = networkTableInstance.getTable("limelight");
-  private NetworkTableEntry botpose = limelight.getEntry("botpose");
+  private NetworkTable limelight = networkTableInstance.getTable("limelight-huskies");
+  private NetworkTableEntry botpose;
   private NetworkTableEntry stream = limelight.getEntry("stream");
   double[] result;
-  double[] temp = { 0,0,0,0,0,0};
+  double[] temp = {0,0,0,0,0,0};
+  String alliance;
 
   public Limelight() {
+
     stream.setNumber(2);//Pour mettre l'image de la limelight en PiP
-    
+    if (DriverStation.getAlliance() == Alliance.Red) {
+      alliance = "red";
+    }
+
+    else {
+      alliance = "blue";
+    }
+    botpose = limelight.getEntry("botpose_wpi" + alliance);
   }
 
   @Override
   public void periodic() {
-
-    //Valeur complète du Pose 3D
-    SmartDashboard.putString("Pose", getPos().toString());
-
     //L'angle du robot par rapport au AprilTag normalisé en Degree
-    SmartDashboard.putNumber("Angle Camera",Math.toDegrees( getPos().getRotation().getZ()));
+    SmartDashboard.putNumber("Tx Camera", getVisionPosition().getTranslation().getX());
+    SmartDashboard.putNumber("Ty Camera", getVisionPosition().getTranslation().getY());
+    SmartDashboard.putNumber("Rz Camera",Math.toDegrees(getVisionPosition().getRotation().getZ()));
+
   }
 
 //Function qui retourne la position 3D du robot par rapport à un AprilTag
-  public Pose3d getPos(){
+  public Pose3d getVisionPosition() {
     //default for getEntry
 
     result = botpose.getDoubleArray(temp);
 
-    if(result.length == 0)
-      result = temp;
+    
 
     Translation3d tran3d = new Translation3d(result[0], result[1], result[2]);
     
