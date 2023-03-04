469c28a99c92947a0144bc22b63b8c7817c08a9d
Huskies Prog <huskies5439@polych.ca>
2023-03-04 15:42:00 -0500

    ajout de Pico pour capteur couleur

--- src/main/java/frc/robot/subsystems/CapteurCouleur.java
+++ src/main/java/frc/robot/subsystems/CapteurCouleur.java
@@ -6,23 +6,25 @@ package frc.robot.subsystems;
 
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
-import com.revrobotics.ColorSensorV3;
 
-import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.PicoColorSensor;
+import frc.robot.PicoColorSensor.RawColor;
 
 public class CapteurCouleur extends SubsystemBase {
 
   // Capteur de couleur
-  ColorSensorV3 capteurCouleur = new ColorSensorV3(I2C.Port.kOnboard);
+  PicoColorSensor capteurCouleur = new PicoColorSensor();
   private final ColorMatch colorMatcher = new ColorMatch();
-  private final Color kCouleurCone = new Color(0.359, 0.485, 0.158);
-  private final Color kCouleurCube = new Color(0.26, 0.429, 0.311);
+  private final Color kCouleurCone = new Color(0.359, 0.158, 0.485 );
+  private final Color kCouleurCube = new Color(0.26, 0.311, 0.429 );
 
   String colorString;
   ColorMatchResult comparaisonCouleur;
+  LinearFilter filter = LinearFilter.singlePoleIIR(0.2, 0.02);
 
   public CapteurCouleur() {
     colorMatcher.addColorMatch(kCouleurCube);
@@ -31,16 +33,20 @@ public class CapteurCouleur extends SubsystemBase {
 
   @Override
   public void periodic() {
-   /*SmartDashboard.putNumber("Proximite", capteurCouleur.getProximity());
-    SmartDashboard.putNumber("Rouge", getCouleur().red);
+   SmartDashboard.putString("Proximite", String.format("%.1f", getProximite()));
+   SmartDashboard.putNumber("Rouge", getCouleur().red);
     SmartDashboard.putNumber("Vert", getCouleur().green);
-    SmartDashboard.putNumber("Bleu", getCouleur().blue);*/
+    SmartDashboard.putNumber("Bleu", getCouleur().blue);
+    if(comparaisonCouleur != null)
+      SmartDashboard.putNumber("Confiance", comparaisonCouleur.confidence); 
     SmartDashboard.putBoolean("Cone?", isCone());
     SmartDashboard.putBoolean("Cube?", isCube());
   }
 
   public Color getCouleur() {
-    return capteurCouleur.getColor();
+    RawColor rawColor = capteurCouleur.getRawColor0();
+    double mag = rawColor.red + rawColor.green + rawColor.blue;
+    return new Color(rawColor.red/mag, rawColor.green/mag, rawColor.blue/mag);
   }
 
   public Color comparerCouleur() {
@@ -48,8 +54,12 @@ public class CapteurCouleur extends SubsystemBase {
     return comparaisonCouleur.color;
   }
 
+  public double getProximite() {
+    return filter.calculate(capteurCouleur.getProximity0());
+  }
+
   public boolean isDetected() {
-    return capteurCouleur.getProximity() > 115;
+    return getProximite() > 115.0;
   }
 
   public boolean isCone() {
